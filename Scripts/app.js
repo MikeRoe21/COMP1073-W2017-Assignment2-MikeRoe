e3a38814ea6b96f0916713071606bbb31aa18ec5
Michael Roe <Mike@Michaels-MacBook-Pro.local>
2017-02-08 17:58:20 -0500

    Lab Checkpoint 2

---
+++ Scripts/app.js
@@ -0,0 +1,16 @@
+/* app.js by Mike Roe (200166222) on http://clientsidescriptingmikeroe.azurewebsites.net */
+/* Using JavaScript to write the text on the main page */
+
+var FirstParagraph = document.getElementById("FirstParagraph");
+var Intro = "My name is Mike Roe. I am a first year student just trying to get my name out there.";
+FirstParagraph.textContent = Intro;
+
+var SecondParagraph = document.getElementById("SecondParagraph");
+var Photo = "My Graduating class from last April. I am in the back, 2nd from the left with the long hair.";
+SecondParagraph.textContent = Photo;
+
+
+
+
+
+
